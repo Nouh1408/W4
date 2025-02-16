4900b74fe602d31094d16227d139d631bac3abd6
Nouh1408 <ahmedinouh@gmail.com>
2025-02-16 15:12:55 +0200

    id remember

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css'
-import {createHashRouter, RouterProvider } from 'react-router-dom'
+import {createBrowserRouter, /* createHashRouter */ RouterProvider } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import Layout from './Components/Layout/Layout'
 import About from './Components/About/About'
+import Register from './Components/Register/Register'
+import Login from './Components/Login/Login'
+import Categories from './Components/Categories/Categories'
+import Products from './Components/Products/Products'
+import NotFound from './Components/NotFound/NotFound'
+import Brands from './Components/Brands/Brands'
+import CArt from './Components/CArt/CArt'
+
 
 function App() {
-  const router = createHashRouter([
+  const router = createBrowserRouter([
     {path:"",element:<Layout/>,children:[
       {index:true, element:<Home/>},
       {path:"about",element:<About/>},
+      {path:"register",element:<Register/>},
+      {path:"login",element:<Login/>},
+      {path:"categories",element:<Categories/>},
+      {path:"cart",element:<CArt/>},
+      {path:"brands",element:<Brands/>},
+      {path:"products",element:<Products/>},
+      {path:"*", element:<NotFound/>}
     ]}
   ])
 
---
+++ src/Components/CArt/CArt.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './CArt.module.css'
+
+export default function CArt() {
+  return (
+    <div>CArt</div>
+  )
+}
