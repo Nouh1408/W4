983156bfb8b930b458e6568d681ccf368c29071c
Nouh1408 <ahmedinouh@gmail.com>
2025-02-21 06:31:23 +0200

    session complete

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,18 +2,34 @@ import React from 'react'
 import style from './Login.module.css'
 import { useFormik } from 'formik'
 
+
 export default function Login() {
+
+  function validation(data) {
+    const x = {}
+    if (data.email == "") {
+      x.email = "email is requireds"
+    } else if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test( data.email) ==false){
+      x.email ="email is invalid"
+    }
+    if (data.password == "") {
+      x.password = "password is required"
+    }
+
+    return x
+  }
 
   const formik = useFormik({
     initialValues: {
       email: "",
       password: ""
     },
-    onSubmit: handleLogin
+    onSubmit: handleLogin,
+    validate: validation
   })
   function handleLogin(values) {
-    console.log("hii",values);
-    
+    console.log("hii", values);
+
   }
   return (
     <>
@@ -30,20 +46,35 @@ export default function Login() {
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.email}
-            
+
+
           />
+
+          
           <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
         </div>
+        {
+            formik.errors.email && formik.touched.email && <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              {formik.errors.email}
+            </div>
+
+          }
         <div className="relative z-0 w-full mb-5 group">
           <input type="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
             name="password"
             id="password"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" " />
           <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
         </div>
+        {
+            formik.errors.password && formik.touched.password && <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              {formik.errors.password}
+            </div>
+
+          }
         <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
 
       </form>
