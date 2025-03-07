03c0a7a8203f2ce68f748bcba61b70f27c3691b0
Nouh1408 <ahmedinouh@gmail.com>
2025-03-07 07:40:18 +0200

    ya rab

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import style from './Register.module.css'
 import { useFormik } from 'formik'
 import axios from 'axios';
 import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import { FaSpinner } from 'react-icons/fa';
 
 
 
 export default function Register() {
+  const [isLoading, setisLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
   const validationSchema = Yup.object().shape({
     email:Yup.string().required("email iss required").email("Invalid email"),
@@ -43,36 +47,47 @@ export default function Register() {
     return errors
 
   } */
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      phone:''
-      },
-      onSubmit:handleregister,
-      // validate:validation
-      validationSchema:validationSchema
-        });
-        async function handleregister(value){
-          console.log("hiii",value);
-          
+ const navigate = useNavigate()
+ const formik = useFormik({
+  initialValues: {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    phone:''
+  },
+  onSubmit: (values, { event }) => {
+    event?.preventDefault();
+    console.log("Submitting form...");
+    handleregister(values);
+}
+,validationSchema:validationSchema
+});
+
+        async function handleregister(values){
+          // console.log("hiii",values);
           try{
-            const {data } = await axios.post("https://ecommerce.routemisr.com/api/v1/auth/signup", value)
-            console.log(data);
-            
+            setisLoading(true)
+            const { data } = await axios.post("https://ecommerce.routemisr.com/api/v1/auth/signup", values);
+            if(data.message=="success"){  
+              console.log("hi");
+              //!token
+              navigate("/");
+              }
           }catch(error){
-            console.log(error);
-            
+            setIsError("email sent")
+          } finally{
+            setisLoading(false)
           }
         }
+        
       
   return (
     <div className=''>
   <form onSubmit={formik.handleSubmit} className="w-3/4 mx-auto">
     <h2 className='my-7 text-green-500'>Register Form</h2>
   <div className="relative z-0 w-full mb-5 group">
+  {isError && <div className="text-red-600">{isError}</div>}
     <input onBlur={formik.handleBlur} value={formik.values.email} onChange={formik.handleChange} type="email" name="email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
     <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
   </div>
@@ -126,7 +141,15 @@ export default function Register() {
     
     
   </div>
-  <button type="submit" className="text-black bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+  <button type="submit"
+   className="text-black bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+    {
+    isLoading ?<FaSpinner className="animate-spin" /> :"Submit"
+    }
+   
+   </button>
+
+
 </form>    
     
     </div>
